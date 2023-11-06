4c0c4efd25c410be418d277bac14ed1fea78d680
schance14 <schance.sd14@gmail.com>
2023-11-05 17:40:23 -0700

    created the movie-view component

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,29 @@
+export const MovieView = ({ movie, onBackClick}) => {
+    return (
+        <div>
+            <div>
+                <img src={movie.image}/>
+            </div>
+            <div>
+                <span>Title: </span>
+                <span>{movie.title}</span>
+            </div>
+            <div>
+                <span>Description: </span>
+                <span>{movie.description}</span>
+            </div>
+            <div>
+                <span>Genre: </span>
+                <span>{movie.genre.name}</span>
+                <span>{movie.genre.description}</span>
+            </div>
+            <div>
+                <span>Director: </span>
+                <span>{movie.director.name}</span>
+                <span>{movie.director.description}</span>
+                <span>{movie.director.birth}</span>
+            </div>
+            <button onClick={onBackClick}>Back</button>
+        </div>
+    );
+};
