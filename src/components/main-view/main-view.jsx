a67a1d76b24ee37c75d7585f8d87b56d3bca69d6
schance14 <schance.sd14@gmail.com>
2023-11-18 11:39:43 -0700

    refactor: create fetch request to pull data from api

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -6,6 +6,35 @@ export const MainView = () => {
     const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch('https://film-finder-82ebda24dfc3.herokuapp.com/movies')
+        .then((response) => response.json())
+        .then((data)=> {
+            console.log(data);
+            const moviesFromApi = data.map((movie)=> {
+                return {
+                    id: movie.id,
+                    title: movie.title,
+                    image: movie.image,
+                    description: movie.description,
+                    genre: {
+                        genreName: movie.genre.genreName,
+                        genreDescription: movie.genre.genreDescription,
+                    },
+                    director: {
+                        directorName: movie.director.directorName,
+                        description: movie.director.description,
+                        birth: movie.director.birth,
+                        death:movie.director.death,
+                    },
+
+                };
+            });
+            setMovies(moviesFromApi);
+        });
+    }, []);
+
 
     if (selectedMovie) {
         return (
