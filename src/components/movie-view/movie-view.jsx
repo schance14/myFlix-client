384ec186ab2a8155082e51613dd6eaa7774db175
schance14 <schance.sd14@gmail.com>
2023-11-28 12:12:58 -0700

    chore: implement prettier and update syntax

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,46 +1,46 @@
 import PropTypes from "prop-types";
 
-export const MovieView = ({ movie, onBackClick}) => {
-    return (
-        <div>
-            <div>
-                <img src={movie.Image}/>
-            </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.Title}</span>
-            </div>
-            <div>
-                <span>Description: </span>
-                <span>{movie.Description}</span>
-            </div>
-            <div>
-                <span>Genre: </span>
-                <span>{movie.Genre.Name}</span>
-                <span>{movie.Genre.Description}</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.Director.Name}</span>
-                <span>{movie.Director.Description}</span>
-                <span>{movie.Director.Birth}</span>
-            </div>
-            <button onClick={onBackClick}>Back</button>
-        </div>
-    );
+export const MovieView = ({ movie, onBackClick }) => {
+  return (
+    <div>
+      <div>
+        <img src={movie.Image} />
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.Title}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.Description}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.Genre.Name}</span>
+        <span>{movie.Genre.Description}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.Director.Name}</span>
+        <span>{movie.Director.Description}</span>
+        <span>{movie.Director.Birth}</span>
+      </div>
+      <button onClick={onBackClick}>Back</button>
+    </div>
+  );
 };
 
 MovieView.PropTypes = {
-    movie: PropTypes.shape({
-        image: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        genre: PropTypes.shape({
-            genreName: PropTypes.string.isRequired,
-        }),
-        description: PropTypes.string.isRequired,
-        director: PropTypes.shape({
-            directorName: PropTypes.string.isRequired,
-        }),
-    }).isRequired,
-    onBackClick: PropTypes.func.isRequired
+  movie: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      genreName: PropTypes.string.isRequired,
+    }),
+    description: PropTypes.string.isRequired,
+    director: PropTypes.shape({
+      directorName: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired,
 };
