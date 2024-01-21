14263c8aec204fb1aa0f854a26c21e6aada752c3
schance14 <schance.sd14@gmail.com>
2024-01-20 18:03:54 -0700

    feat: add container bootstrap component to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,17 @@
 import { createRoot } from "react-dom/client";
-
 import { MainView } from "./components/main-view/main-view";
+import Container from "react-bootstrap/Container";
 
 // Import statement to indicate that you need to bundle `./index.scss`
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
-  return <MainView />;
+  return;
+  <Container>
+    <MainView />;
+  </Container>;
 };
 
 // Finds the root of your app
